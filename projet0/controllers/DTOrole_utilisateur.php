0cc72096a91c0f12781c64f4f53f76abe0deb480
AchrafLansari <chopper_rythmik@hotmail.com>
2012-11-19 21:03:16 +0100

    a valider

---
+++ projet0/controllers/DTOrole_utilisateur.php
@@ -0,0 +1,84 @@
+<?php
+include_once '../models/role_utilisateur.php';
+include_once '../models/connexion.php';
+ 
+
+function ajout_roleutilisateur($id_role,$id_utilisateur){
+	try {
+        $db=connect();
+ 
+        $d = new role_utilisateur($id_role,$id_utilisateur);
+        $resultat= $db->prepare("INSERT INTO role_utilisateur (id_role,id_utilisateur) VALUES (?,?)");
+       
+        $resultat->bindValue(1, $d->getRole(), PDO::PARAM_INT) ;    
+        $resultat->bindValue(2, $d->getRole_utilisateur(), PDO::PARAM_INT) ;    
+          
+       
+       
+        $resultat->execute();
+        return true;
+     
+	} catch (PDOException $exc) {
+		echo $exc->getMessage();
+		return false;
+	}  
+}
+
+
+function modifier_eleve($role,$role_utilisateur){
+    try{
+        
+        
+		$db=connect();
+        
+		$d = new role_utilisateur($id_role,$id_utilisateur);
+        $resultat= $db->prepare("UPDATE  role_utilisateur  SET id_role=?  id_utilisateur=? ");
+       
+        $resultat->bindValue(1, $d->getRole(), PDO::PARAM_INT) ;    
+        $resultat->bindValue(2, $d->getRole_utilisateur(), PDO::PARAM_INT) ;    
+          
+       
+        $resultat->execute();
+        return true;
+    }
+    catch (PDOException $exc)
+    {
+        echo $exc->getMessage();
+        return false;
+    }  
+}
+
+function supprimer_eleve($id){
+  try{
+        
+        $db=connect();
+        $resultat= $db->prepare("DELETE FROM  role_utilisateur  WHERE id_role=?");
+        $resultat->bindValue(1,$id,PDO::PARAM_STR);
+        $resultat->execute();
+        return true;   
+    }  
+    catch (PDOException $exc) {
+        
+        echo $exc->getMessage();
+        return false; 
+    }
+}
+
+function afficher_eleves(){
+    try{
+		$db=connect();
+
+
+		$res=$db->prepare('SELECT * FROM role_utilisateur');
+		$res->execute();
+		return $res;
+
+    }
+    catch (PDOException $exc)
+    {
+		echo $exc->getMessage();
+		return false;
+    }  
+}
+
+?>
