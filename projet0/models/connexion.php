3b35df6e8e735cef8be0195677dcc456f136d903
achraf <achraflansari@gmail.com>
2014-10-30 13:02:39 +0100

    nothing

--- projet0/models/connexion.php
+++ projet0/models/connexion.php
@@ -2,7 +2,7 @@
 
 function connect(){
  try{
-        $db = new PDO('mysql:host=localhost;dbname=projet0', 'root', '');
+        $db = new PDO('mysql:host=localhost;dbname=projet0','root', '');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $db;
  }catch (Exception $ex){
